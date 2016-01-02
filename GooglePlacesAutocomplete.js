34827c03dfe29bc32a083ab68adac5022cb4a8e1
Shashi <shashi@Shashis-MacBook-Pro.local>
2016-01-01 19:24:16 -0800

    added prop to change placeholder text color

--- GooglePlacesAutocomplete.js
+++ GooglePlacesAutocomplete.js
@@ -63,6 +63,7 @@ const GooglePlacesAutocomplete = React.createClass({
 
   propTypes: {
     placeholder: React.PropTypes.string,
+    placeholderTextColor: React.PropTypes.string,
     onPress: React.PropTypes.func,
     minLength: React.PropTypes.number,
     fetchDetails: React.PropTypes.bool,
@@ -87,6 +88,7 @@ const GooglePlacesAutocomplete = React.createClass({
   getDefaultProps() {
     return {
       placeholder: 'Search',
+      placeholderTextColor: '#A8A8A8',
       onPress: () => {},
       minLength: 0,
       fetchDetails: false,
@@ -564,6 +566,7 @@ const GooglePlacesAutocomplete = React.createClass({
             onChangeText={onChangeText ? text => {this._onChangeText(text); onChangeText(text)} : this._onChangeText}
             value={this.state.text}
             placeholder={this.props.placeholder}
+            placeholderTextColor={this.props.placeholderTextColor}
             onFocus={onFocus ? () => {this._onFocus(); onFocus()} : this._onFocus}
             clearButtonMode="while-editing"
           />
