566b33303e1fc0a60b9b2399c90d04d8b48d886c
MowMe - Fantastic Lawns <62850483+mowme@users.noreply.github.com>
2020-04-03 10:13:47 -0400

    Create index.d.ts
    Created an index.d.ts file for typescript support.

---
+++ index.d.ts
@@ -0,0 +1,61 @@
+import React from 'react';
+
+export interface QueryProp {
+    key: string,
+    language?: string,
+    origin?: string,
+    types?: string,
+    sessionToken?: string
+}
+
+export interface GooglePlacesAutocompleteProps {
+    listViewDisplayed?: string | boolean | undefined,
+    placeholder?: string,
+    placeholderTextColor?: string,
+    underlineColorAndroid?: string,
+    returnKeyType?: string,
+    keyboardAppearance?: 'default' | 'light' | 'dark',
+    minLength?: number,
+    fetchDetails?: boolean,
+    autoFocus?: boolean,
+    autoFillOnNotFound?: boolean,
+    timeout?: number,
+    query?: QueryProp,
+    GoogleReverseGeocodingQuery?: object,
+    GooglePlacesSearchQuery?: object,
+    GooglePlacesDetailsQuery?: object,
+    styles?: object,
+    textInputProps?: object,
+    enablePoweredByContainer?: boolean,
+    currentLocation?: boolean,
+    currentLocationLabel?: string,
+    nearbyPlacesAPI?: string,
+    enableHighAccuracyLocation?: boolean,
+    filterReverseGeocodingByTypes?: any[],
+    predefinedPlacesAlwaysVisible?: boolean,
+    enableEmptySections?: boolean,
+    listUnderlayColor?: string,
+    debounce?: number,
+    isRowScrollable?: boolean,
+    text?: string,
+    textInputHide?: boolean,
+    suppressDefaultStyles?: boolean,
+    numberOfLines?: number,
+    editable?: boolean
+    predefinedPlaces?: any[],
+
+    // functions
+    onSubmitEditing?: any,
+    onPress?: any,
+    onNotFound?: any,
+    onFail?: any,
+    getDefaultValue?: any,
+    onTimeout?: any,
+    renderDescription?: any,
+    renderRow?: any,
+    renderLeftButton?: any,
+    renderRightButton?: any,
+}
+
+export class GooglePlacesAutocomplete extends React.Component<GooglePlacesAutocompleteProps> {
+}
