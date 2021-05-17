ffadff02c60cb1ebdd8e4906302c2a5c18db9506
cwtedwards <chris.e@getjobber.com>
2021-05-17 10:38:34 -0600

    add logic to hide and show modal depending on request status

--- GooglePlacesAutocomplete.js
+++ GooglePlacesAutocomplete.js
@@ -500,6 +500,7 @@ export const GooglePlacesAutocomplete = forwardRef((props, ref) => {
 
         setListLoaderDisplayed(false);
         if (request.status === 200) {
+          setListViewDisplayed(true);
           const responseJSON = JSON.parse(request.responseText);
           if (typeof responseJSON.predictions !== 'undefined') {
             // if (_isMounted === true) {
@@ -525,6 +526,7 @@ export const GooglePlacesAutocomplete = forwardRef((props, ref) => {
             }
           }
         } else {
+            setListViewDisplayed(false);
           // console.warn("google places autocomplete: request could not be completed or has been aborted");
         }
       };
