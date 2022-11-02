909b5d2ef6720a7db95b9d22f25c79d03e422143
Willham12 <87827270+Willham12@users.noreply.github.com>
2022-11-02 05:41:24 +0100

    fixed event object for onFocus method in textInputProps

--- GooglePlacesAutocomplete.js
+++ GooglePlacesAutocomplete.js
@@ -819,7 +819,7 @@ export const GooglePlacesAutocomplete = forwardRef((props, ref) => {
             placeholder={props.placeholder}
             onFocus={
               onFocus
-                ? () => {
+                ? (e) => {
                     _onFocus();
                     onFocus(e);
                   }
