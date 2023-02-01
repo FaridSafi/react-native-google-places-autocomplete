73a773ee06db613bc8eff963cb149934dc04df35
situchan <Situchandrashil7@gmail.com>
2023-02-01 01:10:24 -0600

    replace TouchableHighlight with Pressable to support hover color

--- GooglePlacesAutocomplete.d.ts
+++ GooglePlacesAutocomplete.d.ts
@@ -394,6 +394,7 @@ interface GooglePlacesAutocompleteProps {
   keyboardShouldPersistTaps?: 'never' | 'always' | 'handled';
   /** use the ListEmptyComponent prop when no autocomplete results are found. */
   listEmptyComponent?: JSX.Element | React.ComponentType<{}>;
+  listHoverColor?: string;
   listUnderlayColor?: string;
   listViewDisplayed?: 'auto' | boolean;
   /** minimum length of text to search */
